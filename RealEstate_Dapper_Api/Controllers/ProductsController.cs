f77954f82056ca592a5c3c4fbd09e661787c4c74
Eyüp Furkan Tüylü <eyupfurkantuylu@gmail.com>
2023-08-08 13:39:54 +0300

    bolum 9 guncellemeleri

---
+++ RealEstate_Dapper_Api/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate_Dapper_Api.Repositories.ProductRepository;
+
+namespace RealEstate_Dapper_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ProductList()
+        {
+            var values = await _productRepository.GetAllProductAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("ProductListWithCategory")]
+        public async Task<IActionResult> ProductListWithCategory()
+        {
+            var values = await _productRepository.GetAllProductWithCategory();
+            return Ok(values);
+        }
+    }
+}
+
