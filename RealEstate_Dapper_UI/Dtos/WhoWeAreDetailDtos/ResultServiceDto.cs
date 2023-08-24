df5315a6fa0c7944e3a9c19b77d09ca9f45dd70b
Eyüp Furkan Tüylü <eyupfurkantuylu@gmail.com>
2023-08-24 22:22:03 +0300

    WhoWeAreDetail Component Data ilişkisi

---
+++ RealEstate_Dapper_UI/Dtos/WhoWeAreDetailDtos/ResultServiceDto.cs
@@ -0,0 +1,8 @@
+namespace RealEstate_Dapper_UI.Dtos.WhoWeAreDetailDtos
+{
+    public class ResultServiceDto
+    {
+        public int ServiceID { get; set; }
+        public int ServiceName { get; set; }
+    }
+}
